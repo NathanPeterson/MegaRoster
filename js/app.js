516eeb20c598dfdac2592abda40e570cf196fab2
NathanPeterson <nalexpeterson@gmail.com>
2016-06-02 11:08:13 -0400

    Add item to list

--- js/app.js
+++ js/app.js
@@ -7,19 +7,23 @@ var megaRoster = {
   },
 
   setupEventListeners: function(){
-    document.querySelector('#studentForm').onsubmit = this.addStudent;
-
+    document.querySelector('#studentForm').onsubmit = this.addStudent.bind(this);
   },
 
   buildListItem: function(studentName){
     var item = document.createElement('li');
     item.innerText = studentName;
-    return li;
+    return item;
   },
 
   addStudent: function(ev){
     ev.preventDefault();
-    //var
+    var form = ev.currentTarget;
+    var studentName = form.studentName.value;
+    var item = this.buildListItem(studentName);
+    var list = document.querySelector('#studentList');
+    list.appendChild(item);
+
 
   },
 
