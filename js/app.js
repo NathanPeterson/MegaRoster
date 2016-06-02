df770d5fa6edf0aeb70de32167e59cfcdc3708e1
NathanPeterson <nalexpeterson@gmail.com>
2016-06-02 10:20:35 -0400

    Setup event Handler for form submition

--- js/app.js
+++ js/app.js
@@ -1,51 +1,23 @@
-$(document).foundation();
+$(document).foundation()
 
-var BasicForm = {
+var megaRoster = {
 
-  init: function() {
-    document.querySelector('form').onsubmit = this.addValuesToDetails;
+  init: function(){
+    this.setupEventListeners();
   },
 
-  buildColorDiv: function(color) {
-    if (color === undefined) {
-      color = 'red';
-    }
-    return '<div style="height: 20px; width: 20px; border-radius: 50%;background-color: ' + color + '"></div>';
+  setupEventListeners: function(){
+    document.querySelector('form').onsubmit = this.addStudent;
+
   },
 
-  buildList: function(listValues) {
-    var dl = document.createElement('dl');
-    dl.style.border = '1px solid blue';
-    dl.appendChild(this.buildListItem('Name:', listValues.name));
-    dl.appendChild(this.buildListItem('Hair Color:', listValues.hairColor));
-    dl.appendChild(this.buildListItem('Age:', listValues.age));
-    dl.appendChild(this.buildListItem('Birth Place:', listValues.birthplace));
-    return dl;
-  },
-
-  buildListItem: function(term, definition) {
-    var li = document.createElement('li');
-    var dt = document.createElement('dt');
-    var dd = document.createElement('dd');
-    dt.innerHTML = term;
-    dd.innerHTML = definition;
-    li.appendChild(dt);
-    li.appendChild(dd);
-    return li;
-  },
-
-  addValuesToDetails: function(ev) {
+  addStudent: function(ev){
     ev.preventDefault();
-    var details = document.querySelector('.details');
-    var obj = {
-      name: this.name.value,
-      age: this.age.value,
-      birthplace: this.birthplace.value,
-      hairColor: BasicForm.buildColorDiv(this.hairColor.value)
-    };
+    consol.log("gdsafdsfa");
 
-    details.appendChild(BasicForm.buildList(obj));
-  }
-};
+  },
 
-BasicForm.init();
+
+}
+
+megaRoster.init();
--- js/megaroster.js
+++ js/megaroster.js
@@ -4,7 +4,18 @@ var megaroster = {
   init: function(){
     var myRoster = document.querySelector('form');
     myRoster.onsubmit = this.addNameToRoster;
- },
+  },
+
+
+  buildDeleteButton: function(){
+   return '<a style="color: red; border: 3px solid red" href>Delete</a>'
+  },
+
+  buildPromoteButton: function(){
+   return '<a style="color: blue; margin: 125px; border: 3px solid blue" href>Promote</a>'
+  },
+
+
 
   buildList: function(rosterName){
     var dl = document.createElement('dl');
@@ -17,20 +28,25 @@ var megaroster = {
     var li = document.createElement('li');
     var dt = document.createElement('dt');
     var dd = document.createElement('dd');
+    var link = this.buildDeleteButton();
+    var promo = this.buildPromoteButton();
     dt.innerHTML = term;
     dd.innerHTML = definition;
     li.appendChild(dt);
     li.appendChild(dd);
+    li.innerHTML += link;
+    li.innerHTML += promo;
+
     return li;
   },
 
 
- addNameToRoster: function(ev){
-   ev.preventDefault();
-   var details = document.querySelector('.details');
-   var name = this.name.value;
-   details.insertBefore(megaroster.buildList(name), details.childNodes[0]);
-}
+  addNameToRoster: function(ev){
+    ev.preventDefault();
+    var details = document.querySelector('.details');
+    var name = this.name.value;
+    details.insertBefore(megaroster.buildList(name), details.childNodes[0]);
+  }
 };
 
 megaroster.init();
